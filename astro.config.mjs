cefc934da93d48ac951885d0c8669bd217c4a9bc
Zeeshan Ahmed <zahmed@vidiumah.com>
2023-03-04 17:44:25 -0600

    Updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  site: 'https://zshanahmed.github.io',
+  base: '/zshanahmed.github.io',
 });
